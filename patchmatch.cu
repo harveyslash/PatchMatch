3be5aecd78db66893f16ccdda5e6c818755bb1bf
harsh <harshvardhan_gupta@yahoo.com>
2017-12-17 13:02:18 -0500

    Mostly working

--- patchmatch.cu
+++ patchmatch.cu
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <curand_kernel.h>
 
+
+
+extern "C"
+__device__ int get_max(int x,int y){
+    
+    if(x>y)
+        return x;
+    return y;
+}
+
+extern "C"
+__device__ int get_min(int x,int y){
+    
+    if(x<y)
+        return x;
+    return y;
+}
+
+extern "C" 
 __global__ void multiply_them(float *dest, float *a, float *b,int rows,int cols , int channels)
 {   
     int row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -14,7 +34,7 @@ __global__ void multiply_them(float *dest, float *a, float *b,int rows,int cols 
     }
 }
 
-
+extern "C" 
 __global__ void multiply_them_2d(float *dest, float *a, float *b,int rows,int cols)
 {   
     int row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -28,12 +48,13 @@ __global__ void multiply_them_2d(float *dest, float *a, float *b,int rows,int co
 
 
 
-
+extern "C" 
 __device__ void testing(){
     printf("OK");
 }
 
 
+extern "C" 
 __device__ float compute_distance(float *a, 
                                   float *aa,
                                   float *b, 
@@ -87,9 +108,50 @@ __device__ float compute_distance(float *a,
     return pixel_sum / num_points;
        
 }
+extern "C" 
+__device__ void compare_and_update(float *a, 
+                            float *aa,
+                            float *b, 
+                            float *bb,
+                            int rows, 
+                            int cols , 
+                            int channels, 
+                            int patch_size,
+                            int *nnf,
+                            float *nnd,
+                            int x, 
+                            int y,
+                            int bx_new,
+                            int by_new,
+                            int *best_x,
+                            int *best_y,
+                            float *best_d)
+{
+    
+    float dist_new = compute_distance(a,aa,b,bb,rows,cols,channels,patch_size,x,y,bx_new,by_new);
+    
+    if(dist_new < *best_d){
+        *best_d = dist_new; 
+        *best_y = by_new;
+        *best_x = bx_new;
+    }
+    
+    
+                                  
+    
+}
 
+extern "C"
+__device__ float get_rand(){
+    int tId = threadIdx.x + (blockIdx.x * blockDim.x);
+    curandState state;
+    curand_init((unsigned long long)clock() + tId, 0, 0, &state);
+    float rand1 = (float)curand_uniform_double(&state);
+    return rand1;
+    
+}
 
-
+extern "C" 
 __global__ void patch_match(float *a, 
                             float *aa,
                             float *b, 
@@ -103,24 +165,213 @@ __global__ void patch_match(float *a,
                             int iters,
                             int jump_size)
 {
+    
+    
+    printf("%f",get_rand());
+    int xmin, xmax, ymin, ymax;
+
+    
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
-//     compute_distance(a,aa,b,bb,rows,cols,channels,patch_size,col,row,col,row)
     
     
-    nnd[row*cols + col] = compute_distance(a,aa,b,bb,rows,cols,channels,patch_size,col,row,col,row) ; 
+    int init_x = nnf[2*(row*cols + col) + 0 ];
+    int init_y = nnf[2*(row*cols + col) + 1 ];
+
+
+    nnd[row*cols + col] = compute_distance(a,aa,b,bb,rows,cols,channels,patch_size,col,row,init_x,init_y) ; 
     
-    if (row < rows && col < cols){
-        nnf[2*(row*cols + col) ] = 84.0;
-        nnf[2*(row*cols + col) +1] = 84.0;
-    }
+   
+    
 
     for(int i = 0 ; i < iters; i++){
         for(int jump = jump_size ; jump >0 ; jump /=2){
-        
+            
+            int best_x = nnf[2*(row*cols + col) + 0 ];
+            int best_y = nnf[2*(row*cols + col) + 1 ];
+            float best_d = nnd[row*cols + col];
+
+            
+
+            //test up 
+            if (row - jump >=0){
+
+
+//                 int test_px = best_x; 
+//                 int test_py = best_y - jump; 
+                
+                int test_x = nnf[2*(row*cols + col) + 0 ];
+                int test_y = nnf[2*(row*cols + col) + 1 ] + jump;
+
+                if(test_y < rows)
+                {
+                    
+                    compare_and_update(a, 
+                            aa,
+                            b, 
+                            bb,
+                            rows, 
+                            cols , 
+                            channels, 
+                            patch_size,
+                            nnf,
+                            nnd,
+                            col,
+                            row,
+                            test_x,
+                            test_y,
+                            &best_x,
+                            &best_y,
+                            &best_d);
+                              
+                }
+            }
+            
+            
+            
+            if (row + jump < rows){
+
+
+                int test_px = best_x; 
+                int test_py = best_y - jump; 
+                
+                int test_x = nnf[2*(row*cols + col) + 0 ];
+                int test_y = nnf[2*(row*cols + col) + 1 ] - jump;
+
+                if(test_y >=0)
+                {
+                    
+                    compare_and_update(a, 
+                            aa,
+                            b, 
+                            bb,
+                            rows, 
+                            cols , 
+                            channels, 
+                            patch_size,
+                            nnf,
+                            nnd,
+                            col,
+                            row,
+                            test_x,
+                            test_y,
+                            &best_x,
+                            &best_y,
+                            &best_d);
+                              
+                }
+            }
+            
+            //test left
+            if (col - jump >=0){
+
+
+                int test_px = best_x; 
+                int test_py = best_y - jump; 
+                
+                int test_x = nnf[2*(row*cols + col) + 0 ] +jump;
+                int test_y = nnf[2*(row*cols + col) + 1 ];
+
+                if(test_x < cols)
+                {
+                    
+                    compare_and_update(a, 
+                            aa,
+                            b, 
+                            bb,
+                            rows, 
+                            cols , 
+                            channels, 
+                            patch_size,
+                            nnf,
+                            nnd,
+                            col,
+                            row,
+                            test_x,
+                            test_y,
+                            &best_x,
+                            &best_y,
+                            &best_d);
+                              
+                }
+            }
+            
+            //test right
+            if (col + jump < cols){
+
+
+                int test_px = best_x; 
+                int test_py = best_y - jump; 
+                
+                int test_x = nnf[2*(row*cols + col) + 0 ] -jump;
+                int test_y = nnf[2*(row*cols + col) + 1 ];
+
+                if(test_x >=0)
+                {
+                    
+                    compare_and_update(a, 
+                            aa,
+                            b, 
+                            bb,
+                            rows, 
+                            cols , 
+                            channels, 
+                            patch_size,
+                            nnf,
+                            nnd,
+                            col,
+                            row,
+                            test_x,
+                            test_y,
+                            &best_x,
+                            &best_y,
+                            &best_d);
+                              
+                }
+            }
+            
+            int rs_start = 500;
+            if (rs_start > get_max(cols, rows)) {
+                rs_start = get_max(cols, rows);
+            }
+            for (int mag = rs_start; mag >= 1; mag /= 2) {
+                xmin = get_max(best_x - mag, 0), xmax = get_min(best_x + mag + 1, cols);
+                ymin = get_max(best_y - mag, 0), ymax = get_min(best_y + mag + 1, rows);
+                int test_x  = xmin + (int)(get_rand()*(xmax - xmin)) % (xmax - xmin);
+                int test_y = ymin + (int)(get_rand()*(ymax - ymin)) % (ymax - ymin);
+                compare_and_update(a, 
+                            aa,
+                            b, 
+                            bb,
+                            rows, 
+                            cols , 
+                            channels, 
+                            patch_size,
+                            nnf,
+                            nnd,
+                            col,
+                            row,
+                            test_x,
+                            test_y,
+                            &best_x,
+                            &best_y,
+                            &best_d);
+
+                }           
+
+
+        nnf[2*(row*cols + col) + 0] = best_x;
+        nnf[2*(row*cols + col) + 1] = best_y;
+        nnd[1*(row*cols + col) ] = best_d;
+            
+        __syncthreads();
+            
         }
+        
+
         
     }
     
     
 }
+
